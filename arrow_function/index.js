2207767a1d32b1b188b47fc263e9559774a9e214
mouhssine007 <mouhssinemajdi7@gmail.com>
2023-02-23 20:11:12 +0100

    first commit

---
+++ arrow_function/index.js
@@ -0,0 +1,28 @@
+ /*
+ let welcome1 = function (){
+
+    console.log("hola!!!!");
+
+};
+welcome();
+let intro = () =>{
+    console.log(intro)
+}
+
+
+let welcome = (name) =>{
+console.log(`welcome ${name}`);
+}
+welcome("majdiiiiiiii");
+
+*/
+
+let add = (n1,n2) => n1 + n2;
+
+console.log(add(2,5));
+
+let wec = a=>{
+
+    return `name ${a}`;
+};
+console.log(wec("majdiiiiii"));
