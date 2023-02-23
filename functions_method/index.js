a0b2307c37cb836b112e1d673fccb28d8f5f876d
mouhssine007 <mouhssinemajdi7@gmail.com>
2023-02-23 20:23:54 +0100

    first commit

---
+++ functions_method/index.js
@@ -0,0 +1,8 @@
+let welcome = () => "welcome";
+console.log(welcome());
+
+let myName = "majdi";
+console.log(myName.toLowerCase());
+let students = ["majdi","mouhssine"];
+
+console.log(students.push("hamrour"));
