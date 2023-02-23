de73a7b2dca73065e00b2f7860c23d58250d9f65
mouhssine007 <mouhssinemajdi7@gmail.com>
2023-02-23 23:34:42 +0100

    first commit

---
+++ foreach_method/index.js
@@ -0,0 +1,14 @@
+let products = ["bread","milk","honey"];
+
+products.forEach((pro)=>console.log(pro));
+
+//let allProducts= (product)=>console.log(`My products name is ${product}`);
+
+//products.forEach(allProducts);
+
+let students = ["majid","mama","baba"];
+
+students.forEach((student,index)=>{
+console.log(student,index);
+
+});
